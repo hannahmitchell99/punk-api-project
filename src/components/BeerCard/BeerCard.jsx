0be05abdc732e02d710c335ff7c0953f9c5db12b
Hannah <hannahamitchell99@gmail.com>
2023-02-09 18:14:36 +0200

    split description line into paragraphs

--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import "../BeerCard/BeerCard.scss"
+import "../BeerCard/BeerCard.scss";
 
 const BeerCard = (props) => {
-    const {image, name, description} = props
-    return (
-        <div className="beer-card">
-            <img className= "beer-card__image"src={image} alt={name}/>
-            <div className="beer-card__info">
-                <h2 className="beer-card__info--heading">{name}</h2>
-                <p className="beer-card__info--description">{description}</p>
-            </div>
+  const { image, name, description } = props;
+  return (
+    <div className="beer-card">
+      <img className="beer-card__image" src={image} alt={name} />
+      <div className="beer-card__info">
+        <h2 className="beer-card__info--heading">{name}</h2>
+        <div className="beer-card__info--description">
+        {description.match(/[^.!?]+[.!?]+/g).map((sentence, index) => (
+          <p key={name + index}>
+            {sentence}
+          </p>
+        ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default BeerCard
+export default BeerCard;
+
+//p className="beer-card__info--description"{description}
